464694db33751505cab45f9524f2aa4b88242d8f
Jacob Sexton <jshuman@protonmail.com>
2021-08-09 23:38:08 +0000

    Fixed it

--- time_display/app/controllers/times_controller.rb
+++ time_display/app/controllers/times_controller.rb
@@ -1,14 +1,7 @@
-require 'date', 'time'
-
 class TimesController < ApplicationController
 
     def main
-        puts time = Time.now
-        puts @year = time.year
-        puts @month = time.month
-        puts @day = time.day
-        puts @hour = time.hour
-        puts @minute = time.min
+        @time = Time.new
     end
 
 end
